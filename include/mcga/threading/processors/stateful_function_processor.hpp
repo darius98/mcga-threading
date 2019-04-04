84f1f9d487c2b915325d9b49c45757e87c97d58d
Darius Marian <marian.darius98@gmail.com>
2019-04-05 02:51:48 +0300

    * Use std::apply instead of own implementation

--- include/mcga/threading/processors/stateful_function_processor.hpp
+++ include/mcga/threading/processors/stateful_function_processor.hpp
@@ -14,19 +14,10 @@ class StatefulFunctionProcessor {
             args(std::forward<Args>(args)...) {}
 
     void handleObject(const Object& obj) {
-        handleObjectImpl(obj, std::make_index_sequence<numArgs>());
+        std::apply(obj, args);
     }
 
  private:
-    static constexpr std::size_t numArgs
-            = std::tuple_size<std::tuple<Args...>>::value;
-
-    template<std::size_t... S>
-    void handleObjectImpl(const Object& obj,
-                          std::index_sequence<S...> /*unused*/) {
-        obj(std::get<S>(args)...);
-    }
-
     std::tuple<Args...> args;
 };
 
