f8abefad1e30597370ad9952263a29aa093239a4
Darius Marian <marian.darius98@gmail.com>
2019-04-04 20:04:42 +0300

    * Generic WorkerConstruct instead of worker.

--- include/mcga/threading/worker.hpp
+++ include/mcga/threading/worker.hpp
@@ -1,74 +1,30 @@
 #pragma once
 
-#include <mutex>
-#include <thread>
 #include <vector>
 
 #include <concurrentqueue.h>
 
 #include "internal/thread_pool_wrapper.hpp"
 #include "internal/thread_wrapper.hpp"
+#include "internal/worker_construct.hpp"
 
 namespace mcga::threading {
 
-class Worker {
- private:
-    using Executable = std::function<void()>;
+namespace internal {
 
-    Worker();
-    ~Worker() = default;
-
-    DISALLOW_COPY_AND_MOVE(Worker);
-
-    size_t sizeApprox() const;
-
-    void start(volatile std::atomic_bool* running);
-
-    void enqueue(const Executable& job);
-    void enqueue(Executable&& job);
-
-    void run();
-
-    moodycamel::ConcurrentQueue<Executable> queue;
-    moodycamel::ConsumerToken queueConsumerToken;
-    std::vector<Executable> queueBuffer;
-    std::atomic_size_t numBuffered = 0;
-
-friend class internal::ThreadWrapper<Worker>;
-friend class WorkerThread;
-};
-
-class WorkerThread : public internal::ThreadWrapper<Worker> {
+class WorkerExec {
  public:
-    using Executable = Worker::Executable;
+    using Object = std::function<void()>;
 
-    WorkerThread() = default;
-    ~WorkerThread() = default;
-
-    DISALLOW_COPY_AND_MOVE(WorkerThread);
-
-    void enqueue(const Executable& func);
-    void enqueue(Executable&& func);
-
- private:
-    explicit WorkerThread(volatile std::atomic_bool* running):
-            internal::ThreadWrapper<Worker>(running) {}
-
-friend class internal::ThreadPoolWrapper<WorkerThread>;
+    static void handleObject(const Object& obj) {
+        obj();
+    }
 };
 
-class WorkerThreadPool : public internal::ThreadPoolWrapper<WorkerThread> {
- public:
-    using Executable = std::function<void()>;
+} // namespace internal
 
-    using internal::ThreadPoolWrapper<WorkerThread>::ThreadPoolWrapper;
-
-    DISALLOW_COPY_AND_MOVE(WorkerThreadPool);
-
-    ~WorkerThreadPool() = default;
-
-    void enqueue(const Executable& func);
-    void enqueue(Executable&& func);
-};
+using Worker = internal::WorkerConstruct<internal::WorkerExec>;
+using WorkerThread = internal::WorkerThreadConstruct<Worker>;
+using WorkerThreadPool = internal::WorkerThreadPoolConstruct<WorkerThread>;
 
 }  // namespace mcga::threading
