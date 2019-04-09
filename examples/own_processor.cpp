2ade22900e959dcc6d72421a8a7c8774853d6fda
Darius Marian <marian.darius98@gmail.com>
2019-04-09 14:58:47 +0300

    * Make a Pool using own processor

--- examples/own_processor.cpp
+++ examples/own_processor.cpp
@@ -5,8 +5,11 @@
 #include <mcga/threading.hpp>
 
 using mcga::threading::constructs::EventLoopThreadConstruct;
+using mcga::threading::constructs::EventLoopThreadPoolConstruct;
 using std::operator""ms;
 using std::cout;
+using std::endl;
+using std::mutex;
 using std::make_unique;
 using std::unique_ptr;
 
@@ -14,11 +17,17 @@ class Processor {
  public:
     using Task = unique_ptr<int>;
 
-    static void executeTask(Task task) {
-        cout << "Processing " << *task << "\n";
+    mutex printMutex;
+
+    void executeTask(Task task) {
+        std::lock_guard guard(printMutex);
+        cout << "Processing " << *task << " on thread "
+             << std::hash<std::thread::id>()(std::this_thread::get_id())
+             << endl;
     }
 };
 using OwnEventLoopThread = EventLoopThreadConstruct<Processor>;
+using OwnEventLoopThreadPool = EventLoopThreadPoolConstruct<Processor>;
 
 int main() {
     OwnEventLoopThread loop;
@@ -32,6 +41,18 @@ int main() {
     std::this_thread::sleep_for(1000ms);
 
     loop.stop();
+
+    OwnEventLoopThreadPool pool;
+
+    pool.start();
+    pool.enqueueDelayed(make_unique<int>(300), 300ms);
+    for (int i = 1; i <= 100; ++ i) {
+        pool.enqueue(make_unique<int>(i));
+    }
+
+    std::this_thread::sleep_for(1000ms);
+
+    pool.stop();
 
     return 0;
 }
